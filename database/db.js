cf778ddfd6ce17617d650fd5e4f9e0217f15d605
unknown <gofurovmuhammadsolih004@gmail.com>
2024-04-25 11:11:07 +0500

    changes: mysql is connected

---
+++ database/db.js
@@ -0,0 +1,8 @@
+import mysql from 'mysql2';
+
+// const pool = mysql.createPool({
+//     host:,
+//     user:,
+//     database:,
+//     password:,
+// })
--- index.js
+++ index.js
@@ -3,24 +3,39 @@
 // const path = require("path");
 import express from "express";
 import pages from "./routes/pages.js";
-import path, { dirname } from 'path';
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+
+// mysql
+import mysql from "mysql2";
 const app = express();
 const port = 3000;
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home" , {title: "Home page"});
+  res.render("home", { title: "Home page" });
 });
 // Routes
 app.use("/pages", pages);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
+});
+
+// mysql connection
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "secret",
+});
+con.connect(function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected!");
 });
